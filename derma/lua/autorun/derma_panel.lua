b7fe11088a1b0979d64f1756cafb8a951eb223be
Aidan Nellyer <daleagaming@gmail.com>
2017-06-15 16:48:03 +0100

    Update 1.1

--- derma/lua/autorun/derma_panel.lua
+++ derma/lua/autorun/derma_panel.lua
@@ -1,19 +1,45 @@
--- First we create a container, in this case it is a full-screen Derma Frame window.
-local dframe = vgui.Create( 'DFrame' )
-dframe:SetSize( ScrW(), ScrH() )
-dframe:SetTitle( "Information" )
-dframe:Center()
-dframe:MakePopup() -- Enable keyboard and mouse interaction for DFrame panel.
+hook.Add( "Think", "MenuKeyListener", function()
+    if ( input.IsKeyDown( KEY_F2 ) and !menuOpen ) then
+        createMenu()
+    end
+end )
 
--- Create a new DHTML panel as a child of dframe, and dock-fill it.
-local dhtml = vgui.Create( 'DHTML', dframe )
-dhtml:Dock( FILL )
--- Navigate to Garry's Mod wikipedia website.
-dhtml:OpenURL( 'https://wiki.garrysmod.com/index.php' )
--- Run JavaScript code.
-dhtml:Call( [[document.body.innerHTML = 'HTML changed from Lua using JavaScript!';]] )
 
--- This does not throw an error/exception, but instead returns nil/no value.
--- That means you can't pass/return values from JavaScript back to Lua context using this function.
-local number = dhtml:Call( '22;' )
-print( number )
+function createMenu()
+
+  local Board = nil
+  local Name = "Github"
+
+  if not IsValid(Board) then
+    Board = vgui.Create("DFrame")
+    Board:SetSize(750, 500)
+    Board:SetPos(ScrW() / 2 - 325, ScrH() / 2 - 250)
+    Board:SetTitle(Name)
+    Board:Center()
+    Board:MakePopup()
+    Board:SetDraggable(true)
+    Board:ShowCloseButton(true)
+    local MenuPSheet = vgui.Create("DPropertySheet")
+    MenuPSheet:SetParent(Board)
+    MenuPSheet:SetPos(13, 30)
+    MenuPSheet:SetSize(Board:GetWide() - 25, Board:GetTall() - 42)
+    local Laws = vgui.Create("HTML", Board)
+    Laws:SetPos(25, 50)
+    Laws:SetSize(250, 250)
+    local Rules = vgui.Create("HTML", Board)
+    Rules:SetPos(25, 50)
+    Rules:SetSize(250, 250)
+    Rules:OpenURL("www.google.co.uk")
+    local AdminList = vgui.Create("HTML", Board)
+    AdminList:SetPos(25, 50)
+    AdminList:SetSize(250, 250)
+    local order = {}
+    order[3] = {"Laws", Laws, "materials/icon16/user.png", false, false, "Laws of the Land"}
+    order[2] = {"Rules", Rules, "materials/icon16/exclamation.png", false, false, "Rules are listed here"}
+    order[4] = {"Admin List", AdminList, "materials/icon16/shield.png", false, false, "Admins are listed here"}
+  
+    for _, tab in pairs(order) do
+      MenuPSheet:AddSheet(unpack(tab))
+    end
+  end
+end
